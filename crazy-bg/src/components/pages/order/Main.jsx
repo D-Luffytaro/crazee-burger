3d526bff8fd93dab3f05ef7ba44ab78f48bc7aa9
D-Luffytaro <regina_@live.fr>
2024-04-22 20:27:41 +0200

    🚧 F05: ORDER > MAIN > Add layout and grid, Add files and images

--- crazy-bg/src/components/pages/order/Main.jsx
+++ crazy-bg/src/components/pages/order/Main.jsx
@@ -2,15 +2,29 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
   flex: 1;
+  display: grid;
+  grid-template-columns: 25% 1fr;
 
   background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  .basket {
+    background-color: blueviolet;
+  }
+  .menu {
+    background-color: #2196f3;
+  }
 `;
