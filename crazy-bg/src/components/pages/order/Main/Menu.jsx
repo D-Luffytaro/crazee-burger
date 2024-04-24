ac84f85f38c6132335416f7deb97873dc49a5806
D-Luffytaro <regina_@live.fr>
2024-04-24 18:15:56 +0200

    💄 F05: ORDER > MENU > Incorporate responsive design and style for product

--- crazy-bg/src/components/pages/order/Main/Menu.jsx
+++ crazy-bg/src/components/pages/order/Main/Menu.jsx
@@ -24,11 +24,30 @@ export default function Menu() {
 
 const MenuStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  padding: 50px 50px 150px;
+  grid-template-columns: repeat(4, minmax(15.625rem, 1fr));
+  padding: 50px;
   grid-row-gap: 60px;
   justify-items: center;
 
   background: #f5f5f7;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(3, minmax(12.5rem, 1fr));
+  }
+
+  @media (max-width: 992px) {
+    grid-template-columns: repeat(2, minmax(9.375rem, 1fr));
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(12.5rem, 1fr));
+    padding: 3.125rem 1.875rem;
+  }
+
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(auto-fit, minmax(9.375rem, 1fr));
+    padding: 1.875rem;
+    grid-row-gap: 1.875rem;
+  }
 `;
--- crazy-bg/src/components/pages/order/Main/Product.jsx
+++ crazy-bg/src/components/pages/order/Main/Product.jsx
@@ -7,9 +7,11 @@ export default function Product({ imageSource, title, price }) {
         <img src={imageSource} alt={title} />
       </div>
       <div className="description">
-        <div>{title}</div>
-        <div className="price">
-          <div>{price}</div>
+        <div className="title">{title}</div>
+        <div className="priceBtn">
+          <div className="onlyPrice">
+            <div className="price">{price}</div>
+          </div>
           <button>Ajouter</button>
         </div>
       </div>
@@ -18,26 +20,83 @@ export default function Product({ imageSource, title, price }) {
 }
 
 const ProductStyled = styled.div`
-  background-color: #2196f3;
+  //background-color: #2196f3;
   width: 15rem;
   height: 20.625rem;
 
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  border-radius: 15px;
+  box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+
   .image {
-    border: 1px solid red;
-    width: 100px;
-    height: auto;
+    //border: 1px solid red;
+    max-width: 200px;
+    height: 145px;
+
+    display: flex;
+    align-items: center;
   }
 
   img {
     width: 100%;
-    height: 100%;
+    max-height: 145px;
   }
 
   .description {
-    border: 1px solid rebeccapurple;
+    //border: 1px solid red;
+    width: 200px;
+    height: 110px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    padding-bottom: 5px;
+  }
+
+  .title {
+    //border: 1px solid black;
+    font-family: Amatic SC;
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 45.4px;
+    text-align: left;
+    color: #17161a;
+  }
+
+  .priceBtn {
+    //border: 1px solid black;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 190px;
+    height: 66.5px;
+  }
+
+  .onlyPrice {
+    //border: 1px solid red;
   }
 
   .price {
-    border: 1px solid black;
+    font-family: Open Sans;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 22px;
+    text-align: left;
+    color: #ffa01b;
+  }
+
+  button {
+    width: 95px;
+    height: 38px;
+    padding: 12px 26.3px;
+    gap: 0px;
+    border-radius: 5px;
+    background: #ff9f1b;
+    color: white;
+    border: 1px solid #ff9f1b;
   }
 `;
