ee8a4b5ad83f7b23cdf638a63656c327a3a43332
D-Luffytaro <regina_@live.fr>
2024-08-10 01:45:37 +0200

    💄 F08: BUTTON > Add onClick props and style

--- crazy-bg/src/components/reusable-ui/Button.jsx
+++ crazy-bg/src/components/reusable-ui/Button.jsx
@@ -1,9 +1,15 @@
 import styled, { css } from "styled-components";
 import { theme } from "../../theme";
 
-export default function Button({ label, Icon, className, version = "normal" }) {
+export default function Button({
+  label,
+  Icon,
+  className,
+  version = "normal",
+  onClick,
+}) {
   return (
-    <ButtonStyled className={className} version={version}>
+    <ButtonStyled className={className} version={version} onClick={onClick}>
       <span>{label}</span>
       <div className="icon">{Icon && Icon}</div>
     </ButtonStyled>
@@ -11,37 +17,11 @@ export default function Button({ label, Icon, className, version = "normal" }) {
 }
 
 const ButtonStyled = styled.button`
-  width: 100%;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  white-space: nowrap;
-  text-decoration: none;
-  line-height: 1;
-
-  padding: 1.125rem 1.5rem;
-  border-radius: 0.3125rem;
-  font-size: ${theme.fonts.size.SM};
-  font-weight: ${theme.fonts.weights.heavy};
-  color: ${theme.colors.white};
-  background-color: ${theme.colors.primary};
-  border: 0.0625rem solid ${theme.colors.primary};
-
-  .icon {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: ${theme.fonts.size.SM};
-    color: ${theme.colors.white};
-  }
-
   ${({ version }) => extraStyle[version]}
 `;
 
 const extraStylePrimary = css`
   width: 100%;
-  border: 1px solid red;
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -53,19 +33,23 @@ const extraStylePrimary = css`
   padding: 18px 24px;
   border-radius: 5px;
   font-size: 15px;
-  font-weight: 800;
+  font-weight: ${theme.fonts.weights.heavy};
   color: ${theme.colors.white};
   background-color: ${theme.colors.primary};
-  border: ${theme.colors.primary};
+  border: 1px solid ${theme.colors.primary};
 
   &:hover {
     color: ${theme.colors.primary};
     background-color: ${theme.colors.white};
     border: 1px solid ${theme.colors.primary};
     transition: all 200ms ease-out;
+
+    .icon {
+      color: ${theme.colors.primary};
+    }
   }
 
-  :active {
+  &:active {
     background-color: ${theme.colors.primary};
     color: ${theme.colors.white};
   }
@@ -80,12 +64,8 @@ const extraStylePrimary = css`
     border: 1px solid white;
     background-color: ${theme.colors.white};
     color: ${theme.colors.primary};
-    :hover {
-      color: ${theme.colors.white};
-      background-color: ${theme.colors.primary};
-      border: 1px solid ${theme.colors.white};
-    }
-    :active {
+
+    &:active {
       background-color: ${theme.colors.white};
       color: ${theme.colors.primary};
     }
@@ -95,6 +75,8 @@ const extraStylePrimary = css`
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: ${theme.fonts.size.SM};
+    color: ${theme.colors.white};
   }
 `;
 
@@ -117,7 +99,7 @@ const extraStyleSuccess = css`
     border: 1px solid ${theme.colors.success};
   }
 
-  :active {
+  &:active {
     color: ${theme.colors.white};
     background: ${theme.colors.success};
     border: 1px solid ${theme.colors.success};
